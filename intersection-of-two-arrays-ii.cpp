f900c63af284972d226e44b6f428a5b52ce1dada
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-07-22 19:56:50 +0530

    Update intersection-of-two-arrays-ii.cpp

--- intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii.cpp
@@ -5,7 +5,7 @@ public:
     sort(b.begin(), b.end());
     a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
     return a;
-                //
+                
 }
     
 };
